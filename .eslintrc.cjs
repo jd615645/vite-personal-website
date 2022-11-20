850bb4a5814a4283798c52273798b3119dd89a75
JasonTu <jd615645@gmail.com>
2022-11-20 17:44:33 +0000

    feat: update vite config

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,6 +1,12 @@
 /* eslint-env node */
 module.exports = {
   root: true,
+  'env': {
+    'node': true,
+    'commonjs': true,
+    'browser': true,
+    'es6': true
+  },
   'extends': [
     'plugin:vue/vue3-essential',
     'eslint:recommended'
